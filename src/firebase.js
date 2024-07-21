302b240e2ea6b7716b98c454fd18c8b2a8dbf085
Alizadekh <aykhanalizadekh@gmail.com>
2024-07-21 10:13:01 +0400

    Add .env file

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC84sPRDAZbkqZtP71qDCKzxdTYR89PW8g",
-  authDomain: "todochallenge-5ceea.firebaseapp.com",
-  databaseURL: "https://todochallenge-5ceea-default-rtdb.firebaseio.com",
-  projectId: "todochallenge-5ceea",
-  storageBucket: "todochallenge-5ceea.appspot.com",
-  messagingSenderId: "883104790346",
-  appId: "1:883104790346:web:5ec6a6b8437b4bacc6f3b3",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
